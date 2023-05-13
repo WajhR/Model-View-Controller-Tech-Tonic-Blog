48be80ee20f396fc9d03c36728fadf6f2dbee31f
WajhR <w_riyasat@hotmail.com>
2023-05-13 16:37:29 -0400

    fixed the handlebars

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,10 +1,12 @@
+// Get the post ID from the endpoint
 const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
 // Click on edit post
 const editHandler = async (event) => {
     event.preventDefault();
-    const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('textarea[name="post-content"]').value;
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const content = document.querySelector('textarea[name="post-content"]').value.trim();
 
     const response = await fetch(`/api/post/${id}`, {
         method: 'PUT',
