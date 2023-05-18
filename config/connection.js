2bf5f4fe70385e117638abe899d96b35ab38922a
WajhR <w_riyasat@hotmail.com>
2023-05-18 09:26:05 -0400

    corrected sql connection to use jaws

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql'
-  }
+const connection = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql'
+    }
 );
 
 module.exports = connection;
